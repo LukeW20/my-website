3e58682132a795d8acab6be0bf838ffa7decc3fe
Luke Winsley <lukewinsley@gmail.com>
2016-09-28 14:58:46 +0100

    adding content to page

--- articalpage.php
+++ articalpage.php
@@ -1,3 +1,40 @@
+<?php
+
+$id = $_GET['blog'];
+
+require('get_explode_content.php');
+
+$array = [];
+
+class blogPost {
+    public $title;
+    public $text;
+    public $description;
+    public $date;
+    public $author;
+
+    public function __construct($title, $text, $date, $author) {
+        $this->title = $title;
+        $this->text = $text;
+        $this->description = substr($text, 0, 200);
+        $this->date = $date;
+        $this->author = $author;
+    }
+
+    public function getTitle() {
+        return $this->title;
+    }
+}
+
+$array['my-php-blog'] = new blogPost('My PHP Blog',$blogtext[0], '28th Sept 2016', 'Luke Winsley');
+$array['my-java-script-blog'] = new blogPost('My Java-Script Blog', $blogtext[1], '28th Sept 2016', 'Luke Winsley');
+$array['my-html-blog'] = new blogPost('My HTML Blog', $blogtext[2], '28th Sept 2016', 'Luke Winsley');
+$array['my-datebases-blog'] = new blogpost ('My Database Blog', $blogtext[3], '28th Sept 2016', 'Luke Winsley');
+
+$blogpost = $array[$id];
+?>
+
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -18,38 +55,23 @@
         <div class="navbutton"> <a href="aboutme.html"> About Me </a> </div>
         <div class="navbutton"> <a href="portfoliopage.html"> Portfolio </a> </div>
         <div class="navbutton"> <a href="contactpage.html"> Contact </a> </div>
-        <div class="navbutton"> <a href="bloglistpage.html"> Blog </a> </div>
+        <div class="navbutton"> <a href="bloglistpage.php"> Blog </a> </div>
     </div>
 
     <div id="header">
-        <h1> Blog Artical Title </h1>
-    </div>
+        <?php
+        echo "<h1>" . $blogpost->title . "</h1>";
+        ?>
+        </div>
 
     <div id="articalcontainer">
         <div id="articalpageimg"> <img src="images/blogphoto.jpg" alt="Blogger Essentials"> </div>
-        <div id="articalpagecontent"> <h3> Date Posted: 25th Dec 2015 Author: Luke Winsley</h3> <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Nullam commodo tincidunt mollis. Nulla sit amet imperdiet ipsum. Praesent elementum lorem sit amet enim hendrerit lobortis. Donec et magna magna.
-            Morbi hendrerit libero lacinia, fermentum odio ac, cursus lacus. Sed tincidunt dapibus dolor, nec dictum purus cursus non. Maecenas ex sem, pharetra nec lorem vel, viverra consequat arcu.
-            Etiam sollicitudin mi ac sapien dictum, id commodo purus consectetur. Proin sollicitudin est non lectus sagittis iaculis. Maecenas fermentum ultrices consectetur. <br><br>
-
-            Proin magna libero, pulvinar quis ullamcorper eget, consequat nec metus. Proin maximus dolor in urna sollicitudin vulputate. Aliquam at massa vestibulum, volutpat lorem accumsan, luctus urna.
-            Pellentesque sed suscipit lorem. Vivamus porttitor lectus sit amet diam sagittis pulvinar. Mauris ut porttitor nulla, ac pulvinar nunc. Nam viverra tortor et iaculis malesuada. Fusce vel tellus orci.
-            Phasellus mollis augue et dui sollicitudin pulvinar. Praesent ultricies massa nec ipsum rutrum fermentum. In dictum enim eu felis mollis molestie.
-            Duis sit amet urna imperdiet ex convallis placerat eu quis elit. Sed commodo pulvinar metus, id consectetur sem tempor at. In hac habitasse platea dictumst. Fusce condimentum turpis vitae felis aliquam malesuada.<br><br>
-
-            Integer porttitor nisi dapibus urna facilisis pretium. Nulla ultricies nibh eros, at suscipit felis congue bibendum. Suspendisse potenti. Proin commodo eleifend eros nec congue.
-            Suspendisse vehicula maximus turpis, non auctor neque varius vel. In id accumsan neque. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum pharetra diam in sagittis semper.
-            Maecenas odio sapien, interdum quis fringilla quis, maximus eu odio. Praesent eu imperdiet sapien, eget pretium nisi. Fusce pellentesque volutpat lacus, quis tincidunt turpis. Pellentesque vel placerat urna.
-            Phasellus tempus sit amet urna ut luctus. Nulla hendrerit augue quis libero faucibus mattis. Integer dictum orci et faucibus ultrices. Sed nisi mi, porta placerat lectus non, pellentesque suscipit ligula.
-            Nam tristique, est sit amet dignissim scelerisque, nisi tortor sodales lectus, ut tristique mauris sapien sit amet erat. Vivamus molestie vulputate varius. Fusce posuere dui tortor, ac sodales lectus ullamcorper fermentum.<br><br>
-
-            Nullam gravida urna sem, eget finibus est ullamcorper eget. Morbi dignissim elementum laoreet. Praesent mollis quam in lorem maximus tincidunt. Sed cursus sed ipsum quis feugiat.
-            Curabitur ornare arcu ut elit rhoncus, at dictum nulla volutpat. Aenean rutrum vitae libero et viverra. Ut vel nunc ac urna facilisis elementum. Mauris in ante sed arcu lobortis malesuada eget vitae lectus.
-            Nulla facilisi. Nulla eget massa eleifend, luctus enim id, tincidunt nulla. Cras viverra eleifend tortor in ullamcorper. Sed tempor erat lorem, nec pharetra nunc vestibulum a. Aenean rhoncus odio vitae vulputate blandit.
-            Integer massa ligula, euismod sed consequat interdum, tempor et mi. Curabitur turpis orci, accumsan ut tellus ut, rhoncus pretium mi. Pellentesque at sem dapibus, vehicula ex et, pulvinar dui. Quisque ultricies metus quis ipsum rutrum mollis</p> </div>
+        <?php
+                 echo "<div id='articalpagecontent'> <h3>" . $blogpost->date . " - " . $blogpost->author . "</h3> <p>$blogpost->text</p> </div>";
+        ?>
     </div>
     <div id="backbutton">
-        <div> <a href="bloglistpage.html"> Back </a> </div>
+        <div> <a href="bloglistpage.php"> Back </a> </div>
     </div>
     </div>
     <div id="footer">
@@ -60,5 +82,3 @@
     </div>
 </body>
 </html>
-
-
